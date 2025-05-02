f64f96e8dd90d0b3effd075d063df3308f139421
Anka <runner@Mac-1746213257170.local>
2025-05-02 19:25:20 +0000

    bee-away: update URL

--- Casks/bee-away.rb
+++ Casks/bee-away.rb
@@ -1,6 +1,6 @@
 cask "bee-away" do
   version "0.0.1"
-  sha256 "57d741bb3851747d5f0adc1ab07f8500ca5b4bfb5deadcc592f529f7403f9226"
+  sha256 "5e4d223f649f5ac0cee31a98a3cc0dd1eb0e17d47ecf0b7aad7c086525faa006"
 
   url "https://github.com/ragarwalll/bee-away/releases/download/v0.0.1/BeeAway-v0.0.1.dmg"
   name "BeeAway"
